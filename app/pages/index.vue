d6e6b6732ae9edfac992cd3cd4d364c482219293
Sergei <chechet@mail.ru>
2025-10-06 15:53:56 +0400

    main page with scroll

---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <main style="padding: 32px">
+    <h1>Sticker App</h1>
+    <p>Страница демонстрации стикера:</p>
+    <NuxtLink to="/sticker">Открыть стикер</NuxtLink>
+  </main>
+</template>
---
+++ app/pages/sticker.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="sticker-page">
+    <h1 class="title">Демонстрация стикера</h1>
+    <section v-for="i in 7" :key="i" class="section">
+      <h2>Раздел {{ i }}</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis
+        nulla eget nisl dictum luctus. Integer vitae ante eget arcu porttitor
+        suscipit. Quisque dictum, enim nec volutpat gravida, nunc velit pharetra
+        nibh, at luctus sapien nulla ac orci. Suspendisse potenti. Proin tempor
+        viverra justo, ut ultrices arcu luctus sit amet.
+      </p>
+    </section>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.sticker-page {
+  padding: 24px;
+  padding-right: 140px;
+  max-width: 1280px;
+  margin: 0 auto;
+}
+.title { text-align: center; margin: 0 0 16px; }
+.section { margin: 32px 0; }
+</style>
