f970e2ebb315dd4d4009a4acc82a2f3ac2d939b7
Sergei <chechet@mail.ru>
2025-10-08 17:20:56 +0400

    ci: split steps, cache node, conditional artifact upload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-09-29',
   devtools: { enabled: true },
+  nitro: { preset: 'github_pages' },
   css: ['~/assets/styles/main.scss'],
 })
